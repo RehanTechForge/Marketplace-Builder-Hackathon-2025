16d2d72ae65c5559d966bb2230bc055a6fbeb22d
Muhammad Rehan <muhammadrehan125768@gmail.com>
2025-02-03 19:39:01 +0500

    add admin shema to the admin group and the admin

--- Day06/src/sanity/schemaTypes/index.ts
+++ Day06/src/sanity/schemaTypes/index.ts
@@ -6,7 +6,15 @@ import { productType } from "./productType";
 import { customerType } from "./customerType";
 import { orderType } from "./orderType";
 import { orderItemType } from "./orderItemType";
+import { adminType } from "./adminType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, productType, customerType, orderType],
+  types: [
+    blockContentType,
+    categoryType,
+    productType,
+    customerType,
+    orderType,
+    adminType,
+  ],
 };
